82183ae5c39c9008a66e343bfd02fd2c7bfd4745
trevor <trevor_jed_smith@hotmail.com>
2017-07-11 21:14:46 +0930

    Setup of DataContext and MemberContext #1

---
+++ RSCore.Data/Migrations/20170711113313_RSCoreInit.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using RSCore.Data;
+
+namespace RSCore.Data.Migrations
+{
+    [DbContext(typeof(RSCoreDbContext))]
+    [Migration("20170711113313_RSCoreInit")]
+    partial class RSCoreInit
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("RSCore.Models.Product", b =>
+                {
+                    b.Property<int>("ProductID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Category");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<decimal>("Price");
+
+                    b.HasKey("ProductID");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
---
+++ RSCore.Data/RSCoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RSCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSCore.Data
+{
+    public class RSCoreDbContext : DbContext
+    {
+        public RSCoreDbContext(DbContextOptions<RSCoreDbContext> options) : base(options)
+        {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
